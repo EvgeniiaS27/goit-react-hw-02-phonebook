dc54c2798c63e49ac169fd0b72927cd6125f1f4b
EvgeniiaS27 <adm.pr.salionova@gmail.com>
2023-08-01 11:31:19 +0300

    Update component FilterContacts

--- src/components/FilterContacts/FilterContacts.jsx
+++ src/components/FilterContacts/FilterContacts.jsx
@@ -1,11 +1,21 @@
 import PropTypes from 'prop-types';
+import css from './FilterContacts.module.css';
 
 export const FilterContacts = ({ value, onChange }) => {
   return (
-    <label>
-      Finde contacts by name
-      <input type="text" name="filter" value={value} onChange={onChange} />
-    </label>
+    <div className={css.container}>
+      <label htmlFor="idFilter" className={css.label}>
+        Finde contacts by name
+      </label>
+      <input
+        className={css.input}
+        type="text"
+        name="filter"
+        id="idFilter"
+        value={value}
+        onChange={onChange}
+      />
+    </div>
   );
 };
 
